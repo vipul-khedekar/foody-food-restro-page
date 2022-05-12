87be5dc7a7b00e420bcfeb3f3dd4ed5d13b376fa
vipul-khedekar <3kvipul@gmail.com>
2022-05-12 18:45:29 +0530

    added url loader to load assets

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
           {
               test: /\.(png|svg|jpg|jpeg|gif)$/i,
               type: 'asset/resource',
+              use: 'url-loader?mimetype=image/png',
           },
       ],
   },
